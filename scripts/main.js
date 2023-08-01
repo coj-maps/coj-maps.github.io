04dd531522bdd9a58aba16cba91014a6f5056f9c
Jakub Kastner <jakvb.kastner@gmail.com>
2023-08-01 23:57:30 +0200

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,21 @@
+const el = {
+    menuButton: document.querySelectorAll('main .menu .button-round'),
+};
+
+var currentSection = '2023-07';
+
+el.menuButton.forEach(menuButton => menuButton.addEventListener('click', function (event) {
+    const id = event.target.dataset.id;
+
+    const elSectionOld = document.querySelector('main .menu .button-round[data-id="' + currentSection + '"]');
+    elSectionOld.classList.remove('active');
+    const elMenuOld = document.querySelector('main .section[data-id="' + currentSection + '"]');
+    elMenuOld.classList.remove('active');
+
+    currentSection = id;
+
+    const elSectionNew = document.querySelector('main .section[data-id="' + currentSection + '"]');
+    elSectionNew.classList.add('active');
+    const elMenuNew = document.querySelector('main .menu .button-round[data-id="' + currentSection + '"]');
+    elMenuNew.classList.add('active');
+}));
